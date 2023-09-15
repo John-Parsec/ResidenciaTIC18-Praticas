32bf1af9f3eda5d4c2a228f84b88eaf3f6d0632a
Joao Manoel <jmaoliveira.cic@uesc.br>
2023-09-15 10:15:56 -0300

    add exercicio 2

--- src/p13/exercicio2.cpp
+++ src/p13/exercicio2.cpp
@@ -2,7 +2,68 @@
 
 using namespace std;
 
+/*
+Crie um tipo estruturado (struct) empregado para armazenar dados (nome,
+sobrenome, ano de nascimento, RG, ano de admissão, salário) de empregados de
+uma empresa. Defina um vetor de empregados para armazenar até 50 empregados.
+Faça uma função chamada “Reajusta_dez_porcento( )” que receba por parâmetro o
+vetor de empregados e a quantidade de elementos no vetor e atualize o salário de
+cada empregado em 10%. Crie uma função main para testar a função.
+*/
+
+struct empregado{
+    string nome;
+    string sobrenome;
+    int ano_nascimento;
+    string RG;
+    int ano_admissao;
+    float salario;
+};
+
+void geraDados(empregado *empregados, int n){
+    for(int i = 0; i < n; i++){
+        empregados[i].nome = "Nome " + to_string(i);
+        empregados[i].sobrenome = "Sobrenome " + to_string(i);
+        empregados[i].ano_nascimento = 2000 + i;
+        empregados[i].RG = "RG " + to_string(i);
+        empregados[i].ano_admissao = 2010 + i;
+        empregados[i].salario = 1000 + (i*100);
+    }
+}
+
+
+void reajustaDezPorcento(empregado *empregados, int n){
+    for(int i = 0; i < n; i++){
+        empregados[i].salario *= 1.1;
+    }
+}
+
+void imprimeEmpregados(empregado *empregados, int n){
+    for(int i = 0; i < n; i++){
+        cout << "Nome: " << empregados[i].nome << endl;
+        cout << "Sobrenome: " << empregados[i].sobrenome << endl;
+        cout << "Ano de nascimento: " << empregados[i].ano_nascimento << endl;
+        cout << "RG: " << empregados[i].RG << endl;
+        cout << "Ano de admissao: " << empregados[i].ano_admissao << endl;
+        cout << "Salario: " << empregados[i].salario << endl;
+        cout << endl;
+    }
+}  
+
+
 int main(void){
+    empregado empregados[50];
+    int qtdeEmpregados = 5;
+
+    geraDados(empregados, qtdeEmpregados);
+
+    imprimeEmpregados(empregados, qtdeEmpregados);
+
+    cout << "\n----------Reajustando salarios em 10%----------\n" << endl;
+
+    reajustaDezPorcento(empregados, qtdeEmpregados);
+
+    imprimeEmpregados(empregados, qtdeEmpregados);
 
 
     return 0;
--- src/p13/exercicio3.cpp
+++ src/p13/exercicio3.cpp
@@ -2,6 +2,42 @@
 
 using namespace std;
 
+/*
+Faça um programa para um supermercado para a consulta de preço de produtos. O
+programa deverá armazenar de cada produto o seu código, seu nome e seu preço.
+Ao utilizar o programa o usuário deve poder:
+1. Inserir um novo produto
+2. Excluir um produto cadastrado
+3. Listar todos os produtos com seus respectivos códigos e preços
+4. Consultar o preço de um produto através de seu código.
+O código deve ser formado de uma string numérica de 13 caracteres
+O nome deve ter um tamanho de no máximo 20 caracteres
+O preço deve ter duas casas decimais
+O sistema deve permitir o cadastro de até 300 produtos diferentes.
+O sistema deve controlar para que não tenha produtos com o mesmo código ou
+mesmo nome, não permitindo o seu cadastro.
+O sistema deverá ser feito modularizados, com qualidade e utilizando estrutura na
+sua implementação.
+*/
+
+struct produto{
+    string codigo;
+    string nome;
+    float preco;
+};
+
+string geraCodigo(int n){
+    char cd[n+1];
+    
+    for(int i = 0; i < n; i++){
+        cd[i] = '0' + (rand() % 10);
+    }
+    cd[n] = '\0';
+
+    string codigo(cd);
+    return codigo;    
+}
+
 int main(void){
 
 
